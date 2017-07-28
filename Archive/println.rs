d00483fa9876a6f9a5ebf70052e9a1e688a65059
matt <matthew-7.7@hotmail.com>
2017-07-28 14:58:05 +0100

    moved things around

---
+++ Archive/println.rs
@@ -0,0 +1,9 @@
+fn main() {
+    
+    format!("Hello, ");
+    print!("Please wait");
+    println!("{} days", 21);
+
+
+    println!("The name's {0}, {1} {0}.", "Bond", "James");
+}
---
+++ Archive/src/main.rs
@@ -0,0 +1,11 @@
+// This is the main function
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
+
+//to compile, execute "rustc hello-world.rs" from command line
+
+//to run execute "./hello-world" from command line
